b333436e3116e36a81d19f1d7cd08e2d901d3e95
Vitaly Egorov <egorov@samsonos.com>
2014-11-17 15:58:32 +0200

    Fixed PSR styles Added trim when passing object toView()

--- src/Record.php
+++ src/Record.php
@@ -8,38 +8,52 @@ namespace samson\activerecord;
  */
 class Record implements \samson\core\iModuleViewable, \ArrayAccess
 {
-	/** Collection of class fields that would not be passed to module view */
-	public static $restricted = array( 'attached', 'onetoone', 'onetomany', 'class_name' );
-	
-	/** Record primary field value - identifier */
-	public $id;
-	
-	/** @see \samson\core\iModuleViewable::toView() */
-	public function toView( $prefix = NULL, array $restricted = array() )
-	{
-		// Create resulting view data array, add identifier field
-		$values = array( $prefix.'id' => $this->id );
-	
-		// Учтем поля которые не нужно превращать в массив
-		$restricted = array_merge( self::$restricted, $restricted );
-	
-		// Пробежимся по переменным класса
-		foreach( get_object_vars( $this ) as $var => $value )
-		{
-			// Если это не системное поле записи - запишем его значение
-			if( ! in_array( $var, $restricted ) ) $values[ $prefix.$var ] = $value;
-		}	
-	
-		// Вернем массив атрибутов представляющий запись БД
-		return $values;
-	}	
-	
-	/** @see ArrayAccess::offsetSet() */
-	public function offsetSet( $offset, $value ){ $this->$offset = $value; }
-	/** @see ArrayAccess::offsetGet() */
-	public function offsetGet($offset){ return $this->$offset;  }
-	/** @see ArrayAccess::offsetUnset() */
-	public function offsetUnset($offset){ unset( $this->$offset ); }
-	/** @see ArrayAccess::offsetExists() */
-	public function offsetExists($offset){ return property_exists( $this, $offset ); }
+    /** Collection of class fields that would not be passed to module view */
+    public static $restricted = array( 'attached', 'onetoone', 'onetomany', 'class_name' );
+
+    /** Record primary field value - identifier */
+    public $id;
+
+    /** @see \samson\core\iModuleViewable::toView() */
+    public function toView($prefix = null, array $restricted = array())
+    {
+        // Create resulting view data array, add identifier field
+        $values = array($prefix.'id' => $this->id);
+
+        // Учтем поля которые не нужно превращать в массив
+        $restricted = array_merge(self::$restricted, $restricted);
+
+        // Пробежимся по переменным класса
+        foreach (get_object_vars($this) as $var => $value) {
+            // Если это не системное поле записи - запишем его значение
+            if (!in_array($var, $restricted)) {
+                $values[ $prefix.$var ] = trim($value);
+            }
+        }
+
+        // Вернем массив атрибутов представляющий запись БД
+        return $values;
+    }
+
+    /** @see ArrayAccess::offsetSet() */
+    public function offsetSet($offset, $value)
+    {
+
+        $this->$offset = $value;
+    }
+    /** @see ArrayAccess::offsetGet() */
+    public function offsetGet($offset)
+    {
+        return $this->$offset;
+    }
+    /** @see ArrayAccess::offsetUnset() */
+    public function offsetUnset($offset)
+    {
+        unset( $this->$offset );
+    }
+    /** @see ArrayAccess::offsetExists() */
+    public function offsetExists($offset)
+    {
+        return property_exists($this, $offset);
+    }
 }

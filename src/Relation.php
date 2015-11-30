9bf7d218366bb372bc4bbb51c1c5f4541d07fa0e
Vitaly Iegorov <vitalyiegorov@gmail.com>
2015-11-30 15:44:58 +0200

    Added old class

---
+++ src/Relation.php
@@ -0,0 +1,14 @@
+<?php
+namespace samsonframework\orm;
+
+use \samsonframework\orm\ArgumentInterface;
+
+/**
+ * Database field relation types
+ * @author Vitaly Iegorov <egorov@samsonos.com>
+ * @deprecated Use \samsonframework\orm\ArgumentInterface
+ */
+class Relation implements ArgumentInterface
+{
+
+}
